b7ed982e6cc8ab18dc3fd0b2e62bc7b1507f2081
Tony Brix <tony@brix.ninja>
2019-04-26 09:11:48 -0500

    fix registry key

--- AddPackage.bat
+++ AddPackage.bat
@@ -4,31 +4,31 @@ ECHO Adding to the registry the WakaTime package...
 
 REM SQL SERVER MANAGEMENT STUDIO 2012
 REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\11.0\Packages" > nul 2> nul
-if %errorlevel% equ 1 (
+if %errorlevel% equ 0 (
     REG ADD "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\11.0\Packages\{52d9c3ff-c893-408e-95e4-d7484ec7fa47}" /v SkipLoading /t REG_DWORD /d 1
 )
 
 REM SQL SERVER MANAGEMENT STUDIO 2014
 REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\12.0\Packages" > nul 2> nul
-if %errorlevel% equ 1 (
+if %errorlevel% equ 0 (
     REG ADD "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\12.0\Packages\{52d9c3ff-c893-408e-95e4-d7484ec7fa47}" /v SkipLoading /t REG_DWORD /d 1
 )
 
 REM SQL SERVER MANAGEMENT STUDIO 2016
 REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\13.0\Packages" > nul 2> nul
-if %errorlevel% equ 1 (
+if %errorlevel% equ 0 (
     REG ADD "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\13.0\Packages\{52d9c3ff-c893-408e-95e4-d7484ec7fa47}" /v SkipLoading /t REG_DWORD /d 1
 )
 
 REM SQL SERVER MANAGEMENT STUDIO 2017
 REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\14.0\Packages" > nul 2> nul
-if %errorlevel% equ 1 (
+if %errorlevel% equ 0 (
     REG ADD "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\14.0\Packages\{52d9c3ff-c893-408e-95e4-d7484ec7fa47}" /v SkipLoading /t REG_DWORD /d 1
 )
 
 REM SQL SERVER MANAGEMENT STUDIO 2018
 REG QUERY "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\15.0\Packages" > nul 2> nul
-if %errorlevel% equ 1 (
+if %errorlevel% equ 0 (
     REG ADD "HKEY_CURRENT_USER\Software\Microsoft\SQL Server Management Studio\15.0\Packages\{52d9c3ff-c893-408e-95e4-d7484ec7fa47}" /v SkipLoading /t REG_DWORD /d 1
 )
 
